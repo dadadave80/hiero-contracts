0593b8f8daa9d4671c0b2d666fe204a3480a9386
David Dada <daveproxy80@gmail.com>
2026-03-24 00:37:04 +0100

    Move contracts to src
    Signed-off-by: David Dada <daveproxy80@gmail.com>

---
+++ src/schedule-service/IHederaScheduleService.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity >=0.4.9 <0.9.0;
+pragma experimental ABIEncoderV2;
+
+import "../token-service/IHederaTokenService.sol";
+
+interface IHederaScheduleService {}
